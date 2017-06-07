0320dff5a60f76243c202a267ffa0ee2f1d69c37
xucode2016 <xu8023shuchang@16.com>
2017-06-07 15:12:33 +0800

    first

---
+++ src/com/tankwar/view/MainFrame.java
@@ -0,0 +1,22 @@
+/*
+ * TankWar 0.1 Version
+ * 坦克大战 0.1 Version
+ * 项目创建日期:2017.06.07
+ * 开发人员:	@Xu Wenxuan
+ * 			@Liu Bei
+ * */
+package com.tankwar.view;
+
+import java.awt.Frame;
+
+public class MainFrame extends Frame {
+
+	/**
+	 * 组件定义
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
